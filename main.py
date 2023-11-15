0d4c93a6408465d74ee8bea51d063bb96e709fc7
filipe04developing <76597608+filipe04developing@users.noreply.github.com>
2023-11-15 19:48:57 -0300

    Arquivo do jogo

---
+++ main.py
@@ -0,0 +1,33 @@
+class Carta:
+    class Alagamento:
+        pass
+
+    class Tesouro:
+        class Terra:
+            nome = "A Pedra da Terra"
+            contagem = 0
+        class Vento:
+            nome = "A Estátua de Vento"
+            contagem = 0
+        class Fogo:
+            nome = "O Cristal de Fogo"
+            contagem = 0
+        class Agua:
+            nome = "O Cálice de Oceano"
+            contagem=0
+
+    class Aventureiro:
+        class Explorador:
+            pass
+
+        class Piloto:
+            pass
+
+        class Engenheiro:
+            pass
+
+        class Mergulhador:
+            pass
+
+        class Navegador:
+            pass
