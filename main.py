d41a5497b35be7bc1ee7760e98bb9eae2c566f91
filipe04developing <76597608+filipe04developing@users.noreply.github.com>
2023-11-17 21:44:33 -0300

    Update main.py

--- main.py
+++ main.py
@@ -24,6 +24,51 @@ class Carta:
         class ATALAIA:
             nome = "Atalaia"
             contagem = 0
+        class JARDIM_SUSSUROS:
+            nome = "Jardim dos Sussurros"
+            contagem = 0
+        class JARDIM_UIVOS:
+            nome = "Jardim dos Uivos"
+            contagem = 0
+        class TEMPLO_SOL:
+            nome = "Templo do Sol"
+            contagem = 0
+        class TEMPLO_LUA:
+            nome = "Templo da Lua"
+            contagem = 0
+        class CAVERNA_LAVA:
+            nome = "Caverna de Lava"
+            contagem = 0
+        class CAVERNA_SOMBRAS:
+            nome = "Caverna das Sombras"
+            contagem = 0
+        class OBSERVATORIO:
+            nome = "Observatório"
+            contagem = 0
+        class PANTANO_BRUMAS:
+            nome = "Pântano de Brumas"
+            contagem = 0
+        class ROCHA_FANTASMA:
+            nome = "Rocha Fantasma"
+            contagem = 0
+        class PALACIO_MARES:
+            nome = "Palácio dos Mares"
+            contagem = 0
+        class PENEDO_BALDIO:
+            nome = "Penedo Baldio"
+            contagem = 0
+        class BOSQUE_CARMESIM:
+            nome = "Bosque Carmesim"
+            contagem = 0
+        class DUNAS_ENGANO:
+            nome = "Dunas do Engano"
+            contagem = 0
+        class PONTE_SUSPENSA:
+            nome = "Ponte Suspensa"
+            contagem = 0
+        class LAGOA_PERDIDA:
+            nome = "Lagoa Perdida"
+            contagem = 0
 
     class Tesouro:
         class Terra:
